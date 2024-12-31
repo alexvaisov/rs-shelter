035825c968e9b1a771c0c56e375f3bb2b72dbaa0
alexvaisov <70432321+alexvaisov@users.noreply.github.com>
2024-12-31 20:24:03 +0000

    fixed: modal window

--- modal.js
+++ modal.js
@@ -6,6 +6,11 @@ document.querySelectorAll('.modal-close').forEach(function(element){
     element.onclick = closeModal;
 });
 
+document.querySelectorAll('.modal').forEach(function(element){
+    element.onclick = closeModalWrap;
+});
+
+
 function showModal(){
     let modalId = this.dataset.modal;
     document.querySelector(modalId).classList.remove('hide');
@@ -13,5 +18,10 @@ function showModal(){
 
 function closeModal(){
     let modalId = this.dataset.modal;
-    document.querySelector(modalId).parentElement.classList.add('hide');
+    document.querySelector(modalId).classList.add('hide');
+    
+};
+
+function closeModalWrap(){
+    this.classList.add('hide');
 };
