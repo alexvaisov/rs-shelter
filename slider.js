cb94bc12c71d55b429c38d3de2c10094f9c3193d
alexvaisov <70432321+alexvaisov@users.noreply.github.com>
2025-01-01 19:00:01 +0000

    feat: add slider

---
+++ slider.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let sliderNext = document.getElementById('slider-next');
+    let sliderBack = document.getElementById('slider-back');
+    let container = document.querySelector('.slider');
+    let items = document.querySelectorAll('.card');
+    let currentIndex = 0;
+    const totalItems = items.length;
+    const itemWidth = 279;
+
+    if (sliderNext) {
+        sliderNext.addEventListener('click', function() {
+            if (currentIndex >= items.length - 1) {
+                currentIndex = 0;
+                container.style.transition = 'none';
+                container.style.transform = `translateX(0px)`;
+                setTimeout(function(){
+                    container.style.transition = 'transform 0.5s ease';
+                }, 50);
+            } else {
+                currentIndex++;
+                container.style.transform = `translateX(-${currentIndex * itemWidth}px)`;
+            }
+        });
+    };
+
+    sliderBack.addEventListener('click', function () {
+        if (currentIndex <= 0) {
+            currentIndex = totalItems - 1;
+            container.style.transition = 'none';
+            container.style.transform = `translateX(-${currentIndex * itemWidth}px)`;
+            setTimeout(function() {
+                container.style.transition = 'transform 0.5s ease';
+            }, 50);
+        } else {
+            currentIndex--;
+            container.style.transform = `translateX(-${currentIndex * itemWidth}px)`;
+        }
+    });
+    
+});
