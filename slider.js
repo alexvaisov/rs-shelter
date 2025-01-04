2b283dd67e8190ff4209d7af842a9005f47765aa
alexvaisov <70432321+alexvaisov@users.noreply.github.com>
2025-01-04 18:34:43 +0000

    fixed: our friends section

--- slider.js
+++ slider.js
@@ -5,11 +5,33 @@ document.addEventListener('DOMContentLoaded', function () {
     let items = document.querySelectorAll('.card');
     let currentIndex = 0;
     const totalItems = items.length;
-    const itemWidth = 285;
+    const elementWrapper = document.getElementById('wrapper');
+    const styleWrapper = window.getComputedStyle(elementWrapper);
+    const widthWrapper = styleWrapper.width;
+    console.log(widthWrapper);
+    const elementSlider = document.getElementById('slider');
+    const styleSlider = window.getComputedStyle(elementSlider);
+    const gapSlider = styleSlider.gap;
+    console.log(gapSlider.slice(0, -2));
+    let sum = gapSlider.slice(0, -2) * 1 + 270;
+    const itemWidth = sum;
+    console.log(itemWidth);
+    let stepItem = null;
+    function step() {
+        if ((widthWrapper.slice(0, -2) * 1) <= 279) {
+            stepItem = 1;
+        } else if ((widthWrapper.slice(0, -2) * 1) <= 570) {
+            stepItem = 2;
+        } else {
+            stepItem = 3;
+        }
+    };
+    step();
+    console.log(stepItem);
 
     if (sliderNext) {
         sliderNext.addEventListener('click', function() {
-            if (currentIndex >= items.length - 3) {
+            if (currentIndex >= items.length - stepItem) {
                 currentIndex = 0;
                 container.style.transition = 'none';
                 container.style.transform = `translateX(0px)`;
@@ -25,7 +47,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     sliderBack.addEventListener('click', function () {
         if (currentIndex <= 0) {
-            currentIndex = totalItems - 3;
+            currentIndex = totalItems - stepItem;
             container.style.transition = 'none';
             container.style.transform = `translateX(-${currentIndex * itemWidth}px)`;
             setTimeout(function() {
